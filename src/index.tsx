9ca422225e38522a37985b57785bcc16f976de14
ideally <ideally.dai@zeasn.com>
2019-12-22 00:28:26 +0800

    2019/12/18

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from '@/redux';
+// import { AppContainer } from 'react-hot-loader';
+
+import App from './view/App';
+const render = function ( Component ) {
+   const RootApp = (
+      <Provider store={ store }>
+         <Component />
+      </Provider>
+   );
+
+   ReactDOM.render( RootApp, document.getElementById( 'root' ) );
+};
+
+render( App );
+
+// @ts-ignore
+if ( module.hot ) {
+   // @ts-ignore
+   module.hot.accept( './view/App', () => {
+      console.log( '[ 欲上青天揽明月 ] : Accepting the updated in react!' );
+      render( App );
+   } );
+}
